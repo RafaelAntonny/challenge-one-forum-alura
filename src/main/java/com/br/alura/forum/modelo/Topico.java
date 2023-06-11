2ef9d8023c3e165a2c9c0c49d251a70ac86cea97
Rafael Antonny <rafaelantonny6@gmail.com>
2023-06-11 19:06:57 -0300

    aderindo as regras de negocios no trello

--- src/main/java/com/br/alura/forum/modelo/Topico.java
+++ src/main/java/com/br/alura/forum/modelo/Topico.java
@@ -4,12 +4,10 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
-import com.br.alura.forum.modelo.DTOs.DadosTopico;
-
+import com.br.alura.forum.modelo.DTOs.topico.DadosNovoTopico;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -39,24 +37,23 @@ public class Topico {
 	@Enumerated(EnumType.STRING)
 	private StatusTopico status = StatusTopico.NAO_RESPONDIDO;
 
-    @ManyToOne(fetch = FetchType.LAZY)
+    @ManyToOne
 	@JoinColumn(name = "usuario_id")
 	private Usuario autor;
 
-    @ManyToOne(fetch = FetchType.LAZY)
+    @ManyToOne
 	@JoinColumn(name = "curso_id")
 	private Curso curso;
 
 	@OneToMany(mappedBy = "topico")
 	private List<Resposta> respostas = new ArrayList<>();
 
-	public Topico(DadosTopico dados) {
+	public Topico(DadosNovoTopico dados) {
 		this.titulo = dados.titulo();
 		this.mensagem = dados.mensagem();
-		this.dataCriacao = dados.dataCriacao();
+		this.dataCriacao = LocalDate.now();
 		this.status = dados.status();
-		this.autor = dados.autor();
-		this.curso = dados.curso();
-		this.respostas = dados.respostas();
+		this.autor = new Usuario(dados.autor());
+		this.curso = new Curso(dados.curso());
 	}
 }
