6d61e67f7cc020e8053f96ff8b16be6ccb49c7a9
Rafael Antonny <rafaelantonny6@gmail.com>
2023-06-11 02:25:31 -0300

    criação do CR do CRUD

--- src/main/java/com/br/alura/forum/controllers/TopicoController.java
+++ src/main/java/com/br/alura/forum/controllers/TopicoController.java
@@ -1,6 +1,9 @@
 package com.br.alura.forum.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,5 +23,10 @@ public class TopicoController {
     @PostMapping
     public void cadastrar(@RequestBody DadosTopico dados) {
         repository.save(new Topico(dados));
+    }
+
+    @GetMapping
+    public List<Topico> listar() {
+        return repository.findAll();
     }
 }
--- src/main/java/com/br/alura/forum/controllers/UsuarioController.java
+++ src/main/java/com/br/alura/forum/controllers/UsuarioController.java
@@ -1,6 +1,9 @@
 package com.br.alura.forum.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,7 +14,7 @@ import com.br.alura.forum.modelo.DTOs.DadosUsuario;
 import com.br.alura.forum.repositories.UsuarioRepository;
 
 @RestController
-@RequestMapping("/usuario")
+@RequestMapping("/usuarios")
 public class UsuarioController {
     
     @Autowired
@@ -20,5 +23,10 @@ public class UsuarioController {
     @PostMapping
     public void cadastrar(@RequestBody DadosUsuario dados) {
         repository.save(new Usuario(dados));
+    }
+
+    @GetMapping
+    public List<Usuario> listar() {
+        return repository.findAll();
     }
 }
