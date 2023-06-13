c8539244679246da3f3e655edd8173bd6999f1c9
Rafael Antonny <rafaelantonny6@gmail.com>
2023-06-13 01:19:13 -0300

    Tirando dados desnecessarios a o update DTOs

--- src/main/java/com/br/alura/forum/modelo/DTOs/resposta/DadosAtualizarResposta.java
+++ src/main/java/com/br/alura/forum/modelo/DTOs/resposta/DadosAtualizarResposta.java
@@ -1,11 +1,5 @@
 package com.br.alura.forum.modelo.DTOs.resposta;
 
-import java.time.LocalDate;
-
-import com.br.alura.forum.modelo.DTOs.topico.DadosNovoTopico;
-import com.br.alura.forum.modelo.DTOs.usuario.DadosUsuario;
-
-import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotNull;
 
 public record DadosAtualizarResposta(
@@ -14,14 +8,6 @@ public record DadosAtualizarResposta(
     Long id, 
     @NotNull
     String mensagem,
-    @NotNull
-    @Valid 
-    DadosNovoTopico topico,
-    @NotNull
-    LocalDate dataCriacao,
-    @NotNull
-    @Valid
-    DadosUsuario autor,
     @NotNull 
     Boolean solucao) {
     
--- src/main/java/com/br/alura/forum/modelo/DTOs/topico/DadosAtualizarTopico.java
+++ src/main/java/com/br/alura/forum/modelo/DTOs/topico/DadosAtualizarTopico.java
@@ -1,9 +1,6 @@
 package com.br.alura.forum.modelo.DTOs.topico;
 
 import com.br.alura.forum.modelo.StatusTopico;
-import com.br.alura.forum.modelo.DTOs.curso.DadosCurso;
-import com.br.alura.forum.modelo.DTOs.usuario.DadosUsuario;
-
 import jakarta.validation.constraints.NotNull;
 
 public record DadosAtualizarTopico(
@@ -15,10 +12,6 @@ public record DadosAtualizarTopico(
     @NotNull 
     String mensagem,
     @NotNull 
-    StatusTopico status,
-    @NotNull 
-    DadosUsuario autor,
-    @NotNull 
-    DadosCurso curso) {
+    StatusTopico status) {
     
 }
