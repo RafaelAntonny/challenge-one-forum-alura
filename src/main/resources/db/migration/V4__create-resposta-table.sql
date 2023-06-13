d357344eb880095b0b918e8a4c981a26d51fe31a
Rafael Antonny <rafaelantonny6@gmail.com>
2023-06-13 04:08:00 -0300

    tinha esquecido de mudar de usuario para autor

--- src/main/resources/db/migration/V4__create-resposta-table.sql
+++ src/main/resources/db/migration/V4__create-resposta-table.sql
@@ -4,10 +4,10 @@ create table respostas (
     mensagem varchar(2500) not null,
     topico_id bigint not null,
     data_criacao date not null,
-    usuario_id bigint not null,
+    autor_id bigint not null,
     solucao tinyint not null,
 
     primary key(id),
     constraint fk_respostas_topico_id foreign key(topico_id) references topicos(id) on delete cascade,
-    constraint fk_respostas_usuario_id foreign key(usuario_id) references usuarios(id) on delete cascade
+    constraint fk_respostas_usuario_id foreign key(autor_id) references usuarios(id) on delete cascade
 );
